05d960da22c8f2f29a22ec3530ffe2e0b51ea208
matsumon <35322253+matsumon@users.noreply.github.com>
2021-06-11 23:42:29 -0700

    Delete yellow.sh

--- yellow.sh
+++
@@ -1,2 +0,0 @@
-# ./bin/cache_sim ./resources/testconfig /nfs/farm/classes/eecs/spring2021/cs472/public/tracefiles/openssltracefile_1_core0
- ./bin/cache_sim ./resources/testconfig ./resources/openssltracefile_1_core0
